c3179c19d1489a0611cf0c5306b53823aed322c1
Rahul <129066661+ranarahul23@users.noreply.github.com>
2024-01-13 14:46:39 +0530

    quicksort.cpp

--- C++ DSA/quicksort.cpp
+++ C++ DSA/quicksort.cpp
@@ -1,62 +1,38 @@
-#include<iostream>
-using namespace std;
-int partition(int arr[],int s,int e){
-    int pivot=arr[s];
-    
-    int cnt=0;
+int partition( int arr[], int s, int e) {
 
+    int pivot = arr[s];
 
-    for(int i=s+1;i<=e;i++){
-        if(arr[s]>arr[i]){
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
             cnt++;
         }
-
-       
     }
-        int pivotindex=s+cnt;
-        swap(arr[pivotindex],arr[s]);
-        int i=s;
-        int j=e;
 
-        while(i<pivotindex &&j>pivotindex){
-            
-            while(i<pivot){
-                i++;
-            }
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
 
-            while(j>pivot){
-                j--;
-            }
-            if(i<pivot&&j>pivot){
-                swap(arr[i++],arr[j--]);
-            }
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
 
+    while(i < pivotIndex && j > pivotIndex) {
+
+        while(arr[i] <= pivot) 
+        {
+            i++;
         }
 
-        return pivotindex;
+        while(arr[j] > pivot) {
+            j--;
+        }
 
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
 
-}
-void quicksort(int arr[],int s,int e){
-    if(s>=e){
-        return;
-    }
-    int p=partition(arr,s,e);
-    
-    quicksort(arr,s,p-1);
-
-    quicksort(arr,p+1,e);
-
-}
-
-
-int main(){
-    int arr[5]={1,23,2,4,3};
-    int n=5;
-   quicksort(arr,0,n-1);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<endl;
     }
 
-return 0;
+    return pivotIndex;
+
 }
